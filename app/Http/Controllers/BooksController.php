8ef8355a017aa26b3416013e135cdccdc3d5e51d
Artur Guitelar <artur.guitelar@gmail.com>
2020-12-03 16:19:15 -0300

    Testando se um livro pode ser deletado. Redirecionando rotas após criar, atualizar e destruir um registro de livro.

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -9,12 +9,20 @@ class BooksController extends Controller
 {
     public function store(Request $request)
     {
-        Book::create($this->validateRequest($request));
+        $book = Book::create($this->validateRequest($request));
+        return redirect('/books/' . $book->id);
     }
 
     public function update(Book $book, Request $request)
     {
         $book->update($this->validateRequest($request));
+        return redirect('/books/' . $book->id);
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect('/books');
     }
 
     protected function validateRequest(Request $request)
--- tests/Feature/BookReservationTest.php
+++ tests/Feature/BookReservationTest.php
@@ -13,16 +13,15 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_added_to_the_library()
     {
-        $this->withoutExceptionHandling();
-
         $response = $this->post('/books', [
             'title' => 'Título do Livro',
             'author' => 'Autor'
         ]);
 
-        $response->assertOk();
+        $book = Book::first();
 
         $this->assertCount(1, Book::all());
+        $response->assertRedirect('/books/' . $book->id);
     }
 
     /** @test */
@@ -50,8 +49,6 @@ class BookReservationTest extends TestCase
     /** @test */
     public function a_book_can_be_updated()
     {
-        $this->withoutExceptionHandling();
-
         $this->post('/books', [
             'title' => 'Título de um Livro',
             'author' => 'Autor'
@@ -66,5 +63,26 @@ class BookReservationTest extends TestCase
 
         $this->assertEquals('Novo Título', Book::first()->title);
         $this->assertEquals('Novo Autor', Book::first()->author);
+
+        $response->assertRedirect('/books/' . $book->id);
+    }
+
+    /** @test */
+    public function a_book_can_be_deleted()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/books', [
+            'title' => 'Título de um Livro',
+            'author' => 'Autor'
+        ]);
+
+        $book = Book::first();
+        $this->assertCount(1, Book::all());
+
+        $response = $this->delete('/books/' . $book->id);
+
+        $this->assertCount(0, Book::all());
+        $response->assertRedirect('/books');
     }
 }
