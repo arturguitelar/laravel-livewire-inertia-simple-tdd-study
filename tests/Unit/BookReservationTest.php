f8e6cf3af9d5dd5f5f898c513c758d67599063c1
Killdragon <artur.guitelar@gmail.com>
2020-12-05 19:14:43 -0300

    🚨test: Livro pode ser retornado
    Testa se um livro pode ser retornado à biblioteca

--- tests/Unit/BookReservationTest.php
+++ tests/Unit/BookReservationTest.php
@@ -27,4 +27,21 @@ class BookReservationTest extends TestCase
         $this->assertEquals(now(), Reservation::first()->checked_out_at);
     }
 
+    /** @test */
+    public function a_book_can_be_returned()
+    {
+        $book = Book::factory(Book::class)->create();
+        $user = User::factory(User::class)->create();
+
+        $book->checkout($user);
+
+        $book->checkin($user);
+
+        $this->assertCount(1, Reservation::all());
+        $this->assertEquals($user->id, Reservation::first()->user_id);
+        $this->assertEquals($book->id, Reservation::first()->book_id);
+        $this->assertNotnull(Reservation::first()->checked_in_at);
+        $this->assertEquals(now(), Reservation::first()->checked_in_at);
+    }
+
 }
