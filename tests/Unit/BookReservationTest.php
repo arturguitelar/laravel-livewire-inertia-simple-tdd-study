98699347eba413153a60c706817b218a0ed62717
Killdragon <artur.guitelar@gmail.com>
2020-12-05 17:25:15 -0300

    🔵test: Reserva de livro
    Cria teste para reserva de um livro.

---
+++ tests/Unit/BookReservationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Book;
+use App\Models\Reservation;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BookReservationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_book_can_be_checked_out()
+    {
+        $book = Book::factory(Book::class)->create();
+        $user = User::factory(User::class)->create();
+
+        $book->checkout($user);
+
+        $this->assertCount(1, Reservation::all());
+        $this->assertEquals($user->id, Reservation::first()->user_id);
+        $this->assertEquals($book->id, Reservation::first()->book_id);
+        $this->assertEquals(now(), Reservation::first()->checked_out_at);
+    }
+
+}
