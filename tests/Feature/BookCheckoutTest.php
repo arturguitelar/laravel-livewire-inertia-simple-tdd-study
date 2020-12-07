2ba9f27fbb5e582f6934ba3a4c0df3bb4758fc2c
Killdragon <artur.guitelar@gmail.com>
2020-12-07 20:33:45 -0300

    🚨test: Livro pode ser checado por usuario logado
    Testa se um usuário logado pode fazer checkout de um livro.

---
+++ tests/Feature/BookCheckoutTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Book;
+use App\Models\Reservation;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BookCheckoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_book_can_be_checked_out_by_a_signed_in_user()
+    {
+        $book = Book::factory(Book::class)->create();
+        $user = User::factory(User::class)->create();
+
+        $this->actingAs($user)->post('/checkout/' . $book->id);
+
+        $this->assertCount(1, Reservation::all());
+        $this->assertEquals($user->id, Reservation::first()->user_id);
+        $this->assertEquals($book->id, Reservation::first()->book_id);
+        $this->assertEquals(now(), Reservation::first()->checked_out_at);
+    }
+}
