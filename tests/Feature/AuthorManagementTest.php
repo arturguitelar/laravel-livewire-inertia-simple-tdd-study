aa87f866f0f6d8f9f28139725def4ca6db095723
Killdragon <artur.guitelar@gmail.com>
2020-12-08 17:24:35 -0300

    ✨feature: Valida Author
    Validações para autor.
    Testa se nome e data do nascimento são requeridos.
    Vaida dados no controller.

--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -23,8 +23,6 @@ class AuthorManagementTest extends TestCase
     /** @test */
     public function an_author_can_be_created()
     {
-        $this->withoutExceptionHandling();
-
         // substituindo a data com o Carbon porquê, por algum motivo,
         // o set date do faker não funciona
         $newDate = Carbon::createFromFormat('Y/d/m', '1988/14/05')
@@ -39,5 +37,23 @@ class AuthorManagementTest extends TestCase
         $this->assertCount(1, $author);
         $this->assertInstanceOf(Carbon::class, $author->first()->dob);
         $this->assertEquals('1988/14/05', $author->first()->dob->format('Y/d/m'));
+    }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/authors',
+            array_merge($this->data(), ['name' => '']));
+
+        $response->assertSessionHasErrors('name');
+    }
+
+    /** @test */
+    public function a_dob_is_required()
+    {
+        $response = $this->post('/authors',
+            array_merge($this->data(), ['dob' => '']));
+
+        $response->assertSessionHasErrors('dob');
     }
 }
